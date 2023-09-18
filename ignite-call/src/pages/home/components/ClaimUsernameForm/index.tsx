f965474e5285339a21919d04628caecbccaee4a5
Douglas <portogdouglas@gmail.com>
2023-09-18 11:42:25 -0300

    Utilizando React Hook Form

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,34 @@
 import { Form } from "./style";
 import { TextInput, Button } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import {z} from 'zod'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string()
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  async function handleClaimUsername(data:ClaimUsernameFormData ){
+    console.log(data)
+  }
+
   return (
     <Form as="form">
-      <TextInput crossOrigin="" size="sm" prefix="ignite.com/" placeholder="seu usuário" />
+      <TextInput
+        onSubmit={handleSubmit(handleClaimUsername)}
+        crossOrigin=""
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu usuário"
+        {...register("username")}
+      />
       <Button size="sm" type="submit">
         Reservar
-        <ArrowRight/>
+        <ArrowRight />
       </Button>
     </Form>
   );
