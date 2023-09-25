2b21afcad218c76094f546aad4f60b587ec112cc
Douglas <portogdouglas@gmail.com>
2023-09-25 09:28:51 -0300

    Página de confirmação

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,6 @@
 import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmCalendar'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
